f89eeca99e843ec33267fa196acb5d2d07b7730a
DAVIT-IROMASHVILI <geogeodato@gmail.com>
2024-11-14 15:38:45 +0400

    Homework_JS1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 // Write your code here
+    //    1  
+console.log(21 + 21);
+
+    // 2
+
+console.log(21 * 21);
+
+    // 3
+let name = "davit";
+
+console.log("Hello " + name)
+
+    // 4
+let string1 = "lorem";
+let string2 = "ipsum";
+
+console.log(string1 + " " + string2);
